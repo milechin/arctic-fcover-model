a5eb2ad1d0453af201960e83e4c7b31236a96e09
Dennis Milechin <42742721+milechin@users.noreply.github.com>
2025-05-01 09:57:41 -0400

    Added additional xarray tests.

--- MC_resampling/tests/test_lc_trends.py
+++ MC_resampling/tests/test_lc_trends.py
@@ -100,23 +100,9 @@ def test_trend_with_xarray_with_multiple_coords():
         )
   
     # Check the results
-    expected_slope = np.array([[1., 1., 1., 1., 1.],
-                               [1., 1., 1., 1., 1.],
-                               [1., 1., 1., 1., 1.],
-                               [1., 1., 1., 1., 1.],
-                               [1., 1., 1., 1., 1.]])
-
-    expected_intercept = np.array([[1., 1., 1., 1., 1.],
-                                   [1., 1., 1., 1., 1.],
-                                   [1., 1., 1., 1., 1.],
-                                   [1., 1., 1., 1., 1.],
-                                   [1., 1., 1., 1., 1.]])
-
-    expected_p = np.array([[0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0]])
+    expected_slope = np.full((5, 5), 1.0)
+    expected_intercept = np.full((5, 5), 1.0)
+    expected_p = np.full((5, 5), 0)
 
     # Slope
     npt.assert_array_equal(result[0].values, expected_slope)
@@ -126,3 +112,102 @@ def test_trend_with_xarray_with_multiple_coords():
 
     # P-value
     npt.assert_allclose(result[2].values, expected_p, atol=1e-1)
+
+
+def test_trend_with_xarray_with_nan():
+    # Let's create some random data
+    data = np.full((10, 5, 5), np.nan)
+    longitude = np.arange(30, 35, 1)
+    latitude = np.arange(20, 25, 1)
+    time = pd.date_range("2020-01-01", periods=10)
+
+    da = xr.DataArray(
+                        data,
+                        coords={"lon": longitude, 
+                                "lat": latitude, 
+                                "time": time},
+                        dims=("time", "lat", "lon")
+                    )
+
+    result = xr.apply_ufunc(
+        trend,
+        da,
+        input_core_dims=[["time"]],
+        output_core_dims=[[], [], []],
+        vectorize=True,
+        dask="allowed",
+        output_dtypes=[float, float, float]
+        )
+    
+    # Check the results
+    expected_slope = np.full((5, 5), -8888)
+    expected_intercept = np.full((5, 5), -8888)
+    expected_p = np.full((5, 5), -8888)
+
+    # Slope
+    npt.assert_array_equal(result[0].values, expected_slope)
+
+    # Intercept
+    npt.assert_array_equal(result[1].values, expected_intercept)
+
+    # P-value
+    npt.assert_allclose(result[2].values, expected_p, atol=1e-1)
+
+def test_trend_with_xarray_multiple_bands():
+    # Let's create some random data
+    data1 = np.arange(1, 11).repeat(25).reshape(10, 5, 5)
+    data2 = np.arange(-1, -11, -1).repeat(25).reshape(10, 5, 5)
+    longitude = np.arange(30, 35, 1)
+    latitude = np.arange(20, 25, 1)
+    time = pd.date_range("2020-01-01", periods=10)
+
+    da1 = xr.DataArray(
+                        data1,
+                        coords={"lon": longitude, 
+                                "lat": latitude, 
+                                "time": time},
+                        dims=("time", "lat", "lon")
+                    )
+    da2 = xr.DataArray(
+                        data2,
+                        coords={"lon": longitude, 
+                                "lat": latitude, 
+                                "time": time},
+                        dims=("time", "lat", "lon")
+                    )
+    
+    dataset = xr.Dataset(
+                        {
+                            "data1": da1,
+                            "data2": da2
+                        }
+                    )
+
+    result = xr.apply_ufunc(
+        trend,
+        dataset,
+        input_core_dims=[["time"]],
+        output_core_dims=[[], [], []],
+        vectorize=True,
+        dask="allowed",
+        output_dtypes=[float, float, float]
+        )
+  
+    # Check the results
+    expected_slope_data1 = np.full((5, 5), 1.0)
+    expected_intercept_data1 = np.full((5, 5), 1.0)
+    expected_p_data1 = np.full((5, 5), 0)
+
+    expected_slope_data2 = np.full((5, 5), -1)
+    expected_intercept_data2 = np.full((5, 5), -1)
+    expected_p_data2 = np.full((5, 5), 0)
+
+    # Data 1 Check
+    npt.assert_array_equal(result[0].data1.values, expected_slope_data1)
+    npt.assert_array_equal(result[1].data1.values, expected_intercept_data1)
+    npt.assert_allclose(result[2].data1.values, expected_p_data1, atol=1e-1)
+
+    # Data 2 Check
+    npt.assert_array_equal(result[0].data2.values, expected_slope_data2)
+    npt.assert_array_equal(result[1].data2.values, expected_intercept_data2)
+    npt.assert_allclose(result[2].data2.values, expected_p_data2, atol=1e-1)
